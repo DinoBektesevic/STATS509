cb5616f25c58cd95174d5fe1484362add2457a89
DinoBektesevic <dinob@uw.edu>
2020-10-15 16:31:02 -0700

    Fix counting bug in problem6.

--- STATS509/HW2/HW2Code/problem6.py
+++ STATS509/HW2/HW2Code/problem6.py
@@ -4,23 +4,39 @@ np.random.seed(0)
 
 
 def simulate(nSimulations, p):
+    """Runs n simulations, where each simulation is a double coin toss, keeping
+    track of how many attempts are requires untill the first case of HT or TH
+    is observed.
+
+    Parameters
+    ----------
+    nSimulations : `int`
+        Number of simulations to run
+    p : `float`
+        Probability that a coin toss is either heads or tails.
+
+    Returns
+    -------
+    nAttempts : `array`
+        Number of coin tosses that were required until HT or TH were tossed.
+    """
     nSims = 0
 
     nAttempts = []
     for sim in range(nSimulations):
-        nTries = 1
-        flip1, flip2 = 0, 1
-        while (flip1 != flip2):
-            # stats.binom.rvs(1, 0.2, size=1000)
-            flip1 = stats.binom.rvs(1, 0.2)
-            flip2 = stats.binom.rvs(1, 0.2)
-            nTries += 1
+        nTries = 0
+        while True:
+            flip1 = stats.binom.rvs(1, p)
+            flip2 = stats.binom.rvs(1, p)
+            nTries += 2
+            if flip1 != flip2:
+                break
         nAttempts.append(nTries)
     return nAttempts
 
 
 if __name__ == "__main__":
-    nSimulations = 1000
+    nSimulations = 100000
     p = 0.2
     nAttempts = simulate(nSimulations, p)
-    print(f"Average number of attempts to reach success for all simulations was {np.mean(nAttempts)}")
+    print(f"Average number of coin tosses, across all simulations, until success was {np.mean(nAttempts)}")
