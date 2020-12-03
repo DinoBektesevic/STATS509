c071693cc3fc2b893efafbcf0582b741fcbcdfa2
DinoBektesevic <dinob@uw.edu>
2020-12-02 19:47:20 -0800

    Add code and plots for problem 2.

--- STATS509/HW7/HW7Code/HW7_DinoBektesevic.py
+++ STATS509/HW7/HW7Code/HW7_DinoBektesevic.py
@@ -12,14 +12,36 @@ def problem1():
         means.append(x.mean())
 
     means = np.array(means)
-    print(f"Mean of means: {means.mean()}")
-    print(f"Variance of means: {means.std()**2}")
-    print(f"Mean squared error: {np.mean((means-p)**2)}")
+    print("Problem 1:")
+    print(f"    Mean of means: {means.mean()}")
+    print(f"    Variance of means: {means.std()**2}")
+    print(f"    Mean squared error: {np.mean((means-p)**2)}")
 
     plt.hist(means)
     plt.xlabel(r"$\bar X$")
     plt.show()
 
 
+def problem2():
+    n, lambd = 25, 4
+    nTrials = 10000
+    means = []
+    for i in range(nTrials):
+        x = stats.poisson.rvs(lambd, size=n)
+        means.append(x.mean())
+
+    means = np.array(means)
+    print("Problem 2")
+    print(f"    Mean of means: {means.mean()}")
+    print(f"    Variance of means: {means.std()**2}")
+    print(f"    Mean squared error: {np.mean((means-lambd)**2)}")
+
+    plt.hist(means)
+    plt.xlabel(r"$\bar X$")
+    plt.show()
+
+
+
 if __name__ == "__main__":
     problem1()
+    problem2()
