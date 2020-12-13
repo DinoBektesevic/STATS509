3c9a2a7a319766462afc781ff93a9904ed0bae89
DinoBektesevic <dinob@uw.edu>
2020-12-13 14:12:28 -0800

    Problem 7 code and plots.

--- STATS509/HW8/HW8Code/HW8_DinoBektesevic.py
+++ STATS509/HW8/HW8Code/HW8_DinoBektesevic.py
@@ -13,5 +13,34 @@ def problem6c():
     plt.show()
 
 
+def problem7a():
+    theta = np.arange(0, 6, 1)
+    t = np.arange(0, 8, 1)
+
+    # f vs theta plot for different t
+    for i in t:
+        constT = np.exp(-(i -theta))
+        mask = i >= theta
+        plt.plot(theta[mask], constT[mask], label=f"t={i}")
+    plt.xlabel(r"$\theta$")
+    plt.ylabel(r"$f(t|\theta)$")
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+    # f v t plot for different theta
+    for i in theta:
+        constTheta = np.exp(-(t - i))
+        mask = t >= i
+        plt.plot(t[mask], constTheta[mask], label=fr"$\theta={i}$")
+    plt.xlabel(r"t")
+    plt.ylabel(r"$f(t|\theta)$")
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+
+
 if __name__ == "__main__":
     problem6c()
+    problem7a()
